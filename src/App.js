66cb50199a629cf44011e9d1471d68c578ee73e0
guisantosalves <workguisantos@gmail.com>
2022-06-13 14:05:18 -0400

    making the grid of app and create component Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>WhatsApp clone</h1>
+    <div className="app">
+
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar />
+
+        {/* chat */}
+      </div>
+
     </div>
   );
 }
