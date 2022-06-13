9bdc8593235df0d80ed94d855e0f9b6cc49eb903
guisantosalves <workguisantos@gmail.com>
2022-06-13 13:39:09 -0400

    create and setting the app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBC2nMKZU9hLqqZNTPo-zRPNdll7hCIFG8",
+    authDomain: "whatsapp-clone-react-11e77.firebaseapp.com",
+    projectId: "whatsapp-clone-react-11e77",
+    storageBucket: "whatsapp-clone-react-11e77.appspot.com",
+    messagingSenderId: "819807299249",
+    appId: "1:819807299249:web:66fcb9824ea493d7b46490"
+};
+
+export default app = initializeApp(firebaseConfig)
