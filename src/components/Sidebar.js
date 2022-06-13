2e028cca33dbbdf5b4c511db7404de9c24df8c05
guisantosalves <workguisantos@gmail.com>
2022-06-13 14:55:40 -0400

    creating sidebar and installing the material ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./css/Sidebar.css"
 
 function Sidebar(){
     return(
-        <div>
+        <div className="sidebar">
+
+            <div className="sidebar__header">
+
+            </div>
+
+            <div className="sidebar__search">
+
+            </div>
             
+            <div className="sidebar__chats">
+
+            </div>
         </div>
     )
 }
