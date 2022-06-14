8004a747e34344f1a7a1b51a50f70deebdcb83c6
guisantosalves <workguisantos@gmail.com>
2022-06-14 10:43:09 -0400

    making the sidebar and the chats components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,46 @@
 import React from "react";
-import "./css/Sidebar.css"
+import "./css/Sidebar.css";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { Avatar, IconButton } from "@mui/material";
+import {
+  Chat,
+  DonutLarge,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
 
-function Sidebar(){
-    return(
-        <div className="sidebar">
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
 
-            <div className="sidebar__header">
+          <IconButton>
+            <Chat />
+          </IconButton>
 
-            </div>
-
-            <div className="sidebar__search">
-
-            </div>
-            
-            <div className="sidebar__chats">
-
-            </div>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </div>
-    )
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat" type="text" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+          <h1>Sidebar Chat</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
