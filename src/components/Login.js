6bf5d2e6d6d660f68426b4777929a73e3405c184
guisantosalves <workguisantos@gmail.com>
2022-06-16 16:36:20 -0400

    styling login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
+import { Button } from "@mui/material";
 import React from "react";
+import "./css/Login.css"
 
 function Login() {
+
+    const signIn = () => {
+
+    }
+
     return(
         <div className="login">
-            <h1>Login</h1>
+            <div className="login__container">
+                <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/2062095_application_chat_communication_logo_whatsapp_icon.svg/640px-2062095_application_chat_communication_logo_whatsapp_icon.svg.png"
+                    alt="whats logo"
+                />
+                <div className="login__text">
+                    <h1>sign in</h1>
+                </div>
+
+                <Button type="submit" onClick={signIn}>
+                    sign in with google
+                </Button>
+            </div>
         </div>
     )
 }
